8eae347d9f82babfc5f328bfa9ba1ffc8a6654f7
joao-fmr <107021546+joao-fmr@users.noreply.github.com>
2023-05-25 13:49:44 +0200

    Data Collect - New version of the script and urls.txt Change the script so it gets the stations ID directly from the database. Add new variables to urls.txt : API_STATIONS_URL and GUST_BUTTON_LABEL_CLASS.

--- script/script.py
+++ script/script.py
@@ -39,19 +39,21 @@ now = datetime.now(timezone)
 # Format the date and time as a string
 now = now.strftime("%Y-%m-%d %H:%M:%S")
 
-# Define a dictionary of the station ids with their corresponding names
-stations = {'PRE': 'St-Prex', 'CGI': 'Nyon / Changins', 'GVE': 'Genève / Cointrin'}
 
 """
     ENVIRONMENT VARIABLES FROM "urls.txt"
 """
-apiStoreUrl = os.environ['API_STORE_URL'] # Set the api store method URL
+# API URLs
+apiStoreUrl = os.environ['API_STORE_URL']
+apiStationsUrl = os.environ['API_STATIONS_URL']
+# MeteoSuisse URLs
 meteosuisseUrl = os.environ['METEOSUISSE_URL']
 jsonUrl = os.environ['JSON_URL']
 # Define the xpaths for the elements to extract data from
 divClass = os.environ['DIV_CLASS']
 valueClass = os.environ['VALUE_CLASS']
 dateClass = os.environ['DATE_CLASS']
+gustButtonClass = os.environ['GUST_BUTTON_LABEL_CLASS']
 
 
 """
@@ -77,18 +79,24 @@ def extractValue(driver):
     value = float(value.split()[0]) 
     return value
 
+# Make an HTTP Request to the API to get the stations data in the database 
+stationsResponse = requests.get(apiStationsUrl)
+stations = stationsResponse.json()
 
 data = {}
 
 # Go through the stations dictionary
-for key in stations:
+for station in stations:
     stationData = {}
 
-       # Set the URL for the current station's data page on MeteoSuisse website according to the station ID (key)
-    response = meteosuisseUrl.format(key=key) # format URL using the current station key
+    # station id from the database
+    idStation = station['idStation']
+
+    # Set the URL for the current station's data page on MeteoSuisse website according to the station ID (key)
+    response = meteosuisseUrl.format(key=idStation) # format URL using the current station key
 
     # Make a HTTP GET request to get the data in JSON format for the current station 
-    directionResponse = requests.get(jsonUrl.format(key=key)) # format URL using the current station key
+    directionResponse = requests.get(jsonUrl.format(key=idStation)) # format URL using the current station key
     json = directionResponse.json()
 
     # Load the MeteoSuisse page for the current station in the web driver 
@@ -110,7 +118,7 @@ for key in stations:
     valWindSpeed = extractValue(driver)
 
     # Find and click on button to switch to "gust" tab 
-    button = driver.find_element(By.CSS_SELECTOR, 'label[for="pill_subParams1"]')
+    button = driver.find_element(By.CSS_SELECTOR, gustButtonClass)
     button.click()
 
     # Extract gust value from page 
@@ -119,7 +127,7 @@ for key in stations:
     # Go through series in json_data 
     for serie in json['series']:
         # Check the series id from the json to see if it corresponds to the wind direction series
-        if serie['id'] == f'wind_hour.{key}.fr.series.3':
+        if serie['id'] == f'wind_hour.{idStation}.fr.series.3':
             # Get the last data point in the series and extract its value
             last_data = serie['data'][-1]
             valWindDirection = last_data[1]
@@ -130,21 +138,21 @@ for key in stations:
     stationData['valGust'] = valGust
     stationData["valEntryDate"] = entryDate
     stationData["valStoredDate"] = now
-    stationData["fkStation"] = key
+    stationData["fkStation"] = idStation
     
     # Store the station data in the data dictionary according to the station ID
-    data[key] = stationData
+    data[idStation] = stationData
 
 # Go through the data dictionary and send each sub-dictionary separately to the API
-for key in data:
+for idStation in data:
     # Make an HTTP POST request to insert the data into the database
-    response = requests.post(apiStoreUrl, json=data[key])
+    response = requests.post(apiStoreUrl, json=data[idStation])
 
     # Check the status code of the response
     if response.status_code == 201:
         # If the code is 201, the data was inserted successfully
-        print('Données insérées avec succès')
+        print('Data successfully stored...')
     else:
         # If the code is not 201, there was an error inserting the data and the response text wil be printed
-        print('Erreur lors de l\'insertion des données')
+        print('! Error inserting data !')
         print(response.text)
